65ca973d18cc9dc00d4c90fd4a82f26fbddc4cf1
daffavinel <daffarizkyr476@gmail.com>
2025-09-08 09:19:03 +0700

    update semua file

---
+++ komponen/navbar.php
@@ -0,0 +1,29 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nike Shop</title>
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="bg-light">
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg bg-white border-bottom">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="g.html">NIKE</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navMenu">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="iiindex.html">Men</a></li>
+        <li class="nav-item"><a class="nav-link" href="indw.html">Women</a></li>
+        <li class="nav-item"><a class="nav-link" href="kids.html">Kids</a></li>
+        <li class="nav-item"><a class="nav-link" href="sale.html">Sale</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
