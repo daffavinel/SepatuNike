b290e72ca2dda25c13d8c2e14639171fe92bf8a6
daffavinel <daffarizkyr476@gmail.com>
2026-05-12 08:49:30 +0700

    update semua file

--- komponen/bilbort.php
+++ komponen/bilbort.php
@@ -7,7 +7,7 @@
       <p class="card-text">Nike Air Zoom, Regular 41 White Oblique</p>
       <small>Fd2722-110</small>
     </div>
-    <img src="FD2722-110-1-removebg-preview.png" 
+    <img src="images__1_-removebg-preview.png" 
          class="position-absolute end-0 top-50 translate-middle-y me-3 rounded-3" 
          alt="sepatu" width="250">
   </div>
--- page/home.php
+++ page/home.php
@@ -4,7 +4,7 @@
     <div class="col-6 col-md-3">
       <div class="card h-100">
         <a href="index.php?page=detail">
-          <img src="images-removebg-preview (1).png" class="card-img-top" alt="">
+          <img src="https://static.nike.com/a/images/t_web_pdp_936_v2/f_auto/4f709b72-d5fa-442d-b6df-4c89ee2eec5a/WMNS+AIR+ZOOM+ALPHAFLY+NEXT%25+2.png" class="card-img-top" alt="">
         </a>
         <div class="card-body">
           <h6 class="card-title">ALPHAFLY NEXT%2</h6>
@@ -14,7 +14,7 @@
     </div>
     <div class="col-6 col-md-3">
       <div class="card h-100">
-        <img src="Fd6559-100-1-removebg-preview.png" class="card-img-top" alt="">
+        <img src="https://img.ncrsport.com/img/storage/large/Fd6559-100-1.jpg" class="card-img-top" alt="">
         <div class="card-body">
           <h6 class="card-title">Alphafly Next 2 Kipchoge</h6>
           <p class="fw-bold">Rp 1.223.700</p>
@@ -23,7 +23,7 @@
     </div>
     <div class="col-6 col-md-3">
       <div class="card h-100">
-        <img src="images__1_-removebg-preview.png" class="card-img-top" alt="">
+        <img src="https://sportaways.com/storage/products/5806/sepatu-running-nike-w-revolution-7-fb2208-602-hot-punchcrimson-tint-punch-chaudnuance-pourpre-was7-1.webp" class="card-img-top" alt="">
         <div class="card-body">
           <h6 class="card-title">W Revolution 7</h6>
           <p class="fw-bold">Rp 847.200</p>
@@ -32,7 +32,7 @@
     </div>
     <div class="col-6 col-md-3">
       <div class="card h-100">
-        <img src="MP-17893-SR-1_MP-17893-SR-1-removebg-preview.png" class="card-img-top" alt="">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNQt6KKo0URCva0OrYl0x9rp9c9m0ib0Qb_w&s" class="card-img-top" alt="">
         <div class="card-body">
           <h6 class="card-title">Air Zoom Pegasus 39</h6>
           <p class="fw-bold">Rp 1.190.000</p>
@@ -47,7 +47,7 @@
   <div class="row g-3">
     <div class="col-6 col-md-3">
       <div class="card h-100">
-        <img src="images-removebg-preview (1).png" class="card-img-top" alt="">
+        <img src="https://static.nike.com/a/images/t_web_pdp_936_v2/f_auto/4f709b72-d5fa-442d-b6df-4c89ee2eec5a/WMNS+AIR+ZOOM+ALPHAFLY+NEXT%25+2.png" class="card-img-top" alt="">
         <div class="card-body">
           <h6 class="card-title">ALPHAFLY NEXT%2</h6>
           <p class="fw-bold">Rp 4.899.000</p>
@@ -56,7 +56,7 @@
     </div>
     <div class="col-6 col-md-3">
       <div class="card h-100">
-        <img src="Fd6559-100-1-removebg-preview.png" class="card-img-top" alt="">
+        <img src="https://img.ncrsport.com/img/storage/large/Fd6559-100-1.jpg" class="card-img-top" alt="">
         <div class="card-body">
           <h6 class="card-title">Alphafly Next 2 Kipchoge</h6>
           <p class="fw-bold">Rp 1.223.700</p>
@@ -65,7 +65,7 @@
     </div>
     <div class="col-6 col-md-3">
       <div class="card h-100">
-        <img src="images__1_-removebg-preview.png" class="card-img-top" alt="">
+        <img src="https://sportaways.com/storage/products/5806/sepatu-running-nike-w-revolution-7-fb2208-602-hot-punchcrimson-tint-punch-chaudnuance-pourpre-was7-1.webp" class="card-img-top" alt="">
         <div class="card-body">
           <h6 class="card-title">W Revolution 7</h6>
           <p class="fw-bold">Rp 847.200</p>
@@ -74,7 +74,7 @@
     </div>
     <div class="col-6 col-md-3">
       <div class="card h-100">
-        <img src="MP-17893-SR-1_MP-17893-SR-1-removebg-preview.png" class="card-img-top" alt="">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNQt6KKo0URCva0OrYl0x9rp9c9m0ib0Qb_w&s" class="card-img-top" alt="">
         <div class="card-body">
           <h6 class="card-title">Air Zoom Pegasus 39</h6>
           <p class="fw-bold">Rp 1.190.000</p>
